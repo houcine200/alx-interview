1e8d454295d3fcad6044ede1f9dda21dae059446
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-02 15:26:49 +0100

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,17 +7,28 @@ line_count = 0
 file_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
-for line in stdin:
-    line_count += 1
+try:
+    for line in stdin:
+        line_count += 1
 
-    file_size += int(line.split()[-1])
-    status_code = int(line.split()[-2])
+        file_size += int(line.split()[-1])
+        status_code = int(line.split()[-2])
 
-    if status_code in status_codes:
-        status_codes[status_code] += 1
+        if status_code in status_codes:
+            status_codes[status_code] += 1
 
-    if line_count % 10 == 0:
-        print("File size:", file_size)
-        for code, count in sorted(status_codes.items()):
-            if count > 0:
-                print(f"{code}: {count}")
+        if line_count == 10:
+            print("File size:", file_size)
+            for code, count in sorted(status_codes.items()):
+                if count > 0:
+                    print(f"{code}: {count}")
+
+            line_count = 0
+            file_size = 0
+            status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+except KeyboardInterrupt:
+    print("File size:", file_size)
+    for code, count in sorted(status_codes.items()):
+        if count > 0:
+            print(f"{code}: {count}")
