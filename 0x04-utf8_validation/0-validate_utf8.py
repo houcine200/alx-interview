8c2b226d3848e5d1021c3f590ccdee70919213e4
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-07 12:21:02 +0100

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,12 +7,27 @@
 def validUTF8(data):
     """Return True if data is a valid UTF-8 encoding, else return False.
     """
-    for num in data:
-        if num < 0 or num > 255:
+    count = 0
+
+    for byte in data:
+        if not 0 <= byte <= 255:
             return False
-    try:
-        bytes_data = bytes(data)
-        bytes_data.decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
+
+        if count == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                count = 1
+            elif byte >> 4 == 0b1110:
+                count = 2
+            elif byte >> 3 == 0b11110:
+                count = 3
+            else:
+                return False
+        else:
+            if byte >> 6 == 0b10:
+                count -= 1
+            else:
+                return False
+
+    return count == 0
