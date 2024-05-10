8bc842b42d219cf6eb7a3576b136ba66dcc3b9e2
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-10 13:14:27 +0100

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
+
 """ A script to determines if a given data
     set represents a valid UTF-8 encoding.
 """
 
 
 def validUTF8(data):
-    """Return True if data is a valid UTF-8 encoding, else return False.
+    """
+    Return True if data is a valid UTF-8 encoding, else return False.
     """
     count = 0
-
     for byte in data:
-        if not 0 <= byte <= 255:
-            return False
-
         if count == 0:
-            if byte >> 7 == 0b0:
-                count = 0
-            elif byte >> 5 == 0b110:
+            if (byte >> 5) == 0b110:
                 count = 1
-            elif byte >> 4 == 0b1110:
+            elif (byte >> 4) == 0b1110:
                 count = 2
-            elif byte >> 3 == 0b11110:
+            elif (byte >> 3) == 0b11110:
                 count = 3
-            else:
-                return False
+            elif (byte >> 7):
+                count = 0
         else:
-            if byte >> 6 == 0b10:
-                count -= 1
-            else:
+            if (byte >> 6) != 0b10:
                 return False
-
+            count -= 1
     return count == 0
