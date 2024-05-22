f39b03dda8a5afb7dada81b2101be322d9f3cd7c
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-22 14:17:47 +0100

    fix task 0 for correct output

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,23 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-const request = require('request');
+const util = require('util');
+const request = util.promisify(require('request'));
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-
-// URL for fetching the movie data
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-// Fetch the movie data
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  const movie = JSON.parse(body);
-  const characters = movie.characters;
-
-  // Fetch and print each character name
-  characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-
-      const character = JSON.parse(body);
+const fetchCharacters = async () => {
+  const args = process.argv.slice(2);
+  if (args.length === 0) return;
+  try {
+    const movieReq = await request(`https://swapi-api.alx-tools.com/api/films/${args[0]}`);
+    const charactersUrls = await JSON.parse(movieReq.body).characters;
+    if (!charactersUrls) return;
+    for (const characterUrl of charactersUrls) {
+      const characterReq = await request(characterUrl);
+      const character = await JSON.parse(characterReq.body);
       console.log(character.name);
-    });
-  });
-});
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
+fetchCharacters();
