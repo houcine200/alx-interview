69bb63aa5d7b1c6c09633dc467da6aca5574b22b
Houcine Walaq <houcine.walaq@gmai.com>
2024-05-22 13:30:50 +0100

    task0

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// URL for fetching the movie data
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+// Fetch the movie data
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        return;
+    }
+
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    // Fetch and print each character name
+    characters.forEach(characterUrl => {
+        request(characterUrl, (error, response, body) => {
+            if (error) {
+                console.error('Error:', error);
+                return;
+            }
+
+            const character = JSON.parse(body);
+            console.log(character.name);
+        });
+    });
+});
